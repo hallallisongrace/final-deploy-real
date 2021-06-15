79da4d76271187fff75612b2d4b0762bd9d63514
Austin <harrenaustint@gmail.com>
2021-06-15 14:24:48 -0400

    push

--- client/src/Viewattra.js
+++ client/src/Viewattra.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import {BACKEND_URL} from './config'
-import '../styles/Attractions.css'
+import 'Viewattrac.css'
 
 
 const Attraction = (props) => {
