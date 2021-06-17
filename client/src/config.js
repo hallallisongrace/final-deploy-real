97455e0132929722f13bbf3c54a62f0b642b9d5d
Austin <harrenaustint@gmail.com>
2021-06-17 10:11:41 -0400

    push

--- client/src/config.js
+++ client/src/config.js
@@ -3,4 +3,4 @@ import dotenv from 'dotenv'
 
 dotenv.config()
 
-export const BACKEND_URL=process.env.NODE_ENV === 'development'? "http://localhost:5000/": "https://allison-deploy.herokuapp.com/"
+export const BACKEND_URL=process.env.NODE_ENV === 'development'? "http://localhost:5000/": "https://new-alli-deploy.herokuapp.com/"
