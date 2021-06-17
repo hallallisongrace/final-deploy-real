c01a7675284dcadf3f4c3890438a824535efa66a
Allison Hall <allison@Allisons-MacBook-Pro.local>
2021-06-17 13:56:51 -0400

    do the thing

--- client/src/config.js
+++ client/src/config.js
@@ -3,4 +3,4 @@ import dotenv from 'dotenv'
 
 dotenv.config()
 
-export const BACKEND_URL=process.env.NODE_ENV === 'development'? "http://localhost:5000/": "https://new-alli-deploy.herokuapp.com/"
+export const BACKEND_URL=process.env.NODE_ENV === 'development'? "http://localhost:5000/": "https://final-deploy-real.herokuapp.com/"
